1b5a81922630b8cb715e472aebb27620ae9e6653
DenisCDev <coldgreen04@gmail.com>
2024-11-03 13:21:35 -0300

    fix: link google drive

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -59,7 +59,7 @@ const HeroSection = () => {
               Contate-me
             </button>
             <Link
-              href="/download/Currículo Denis.pdf"
+              href="https://drive.google.com/file/d/1R9rHx7-q1My-fqVCI5s_TtYNfSM_zwVR/view?usp=sharing"
               target="_blank"
               className="px-8 py-3 w-full sm:w-auto rounded-lg bg-yellow-600 hover:bg-indigo-500 text-white font-semibold text-lg shadow-lg transition-transform transform hover:scale-100 duration-300 mt-3 sm:mt-0"
             >
